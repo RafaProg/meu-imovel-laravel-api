d654f349c7050dbfb61f987aaa20be96e2ba86b5
RafaProg <herminathor@gmail.com>
2020-04-13 17:47:52 -0300

    Update RealStateRepository.php

--- app/Repository/RealStateRepository.php
+++ app/Repository/RealStateRepository.php
@@ -3,22 +3,19 @@ namespace App\Repository;
 
 class RealStateRepository extends AbstractRepository
 {
-	private $location;
+	public function __construct(Model $model, Request $request = null)
+    {
+        parent::__construct($model, $request);
 
-	public function setLocation(array $data): self
-	{
-		$this->location = $data;
-		return $this;
-	}
+        $location;
+        
+        if($request) {
+            $location = $request->all(['city', 'state']);
 
-	public function getResult()
-	{
-		$location = $this->location;
-
-		return $this->model->whereHas('address', function($q) use($location){
-								$q->where('state_id', $location['state'])
-								  ->where('city_id', $location['city'])
-								;
-							});
-	}
+            return $this->model->whereHas('address', function($query) use ($location) {
+                $query->where('state_id', $location['state'])
+                      ->where('city_id', $location['city']);
+            });
+        }
+    }
 }
